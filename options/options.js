52b028f23acf6c98b8fc5358d25e1ba8eee6cd54
Crowdbotics <team@crowdbotics.com>
2023-07-26 18:58:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "groupchat-42444",
+  url: "https://groupchat-42444.botics.co",
+  api: "https://groupchat-42444.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
